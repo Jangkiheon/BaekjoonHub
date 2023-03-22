ac0657eee2a66c46175a8a7ad16b3986590f62ae
Jangkiheon <95671810+Jangkiheon@users.noreply.github.com>
2023-03-22 21:38:42 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a < 101 and a > 89:
+    print('A')
+elif a < 90 and a > 79:
+    print('B')
+elif a < 80 and a > 69:
+    print('C')
+elif a < 70 and a > 59:
+    print('D')
+else:
+    print('F')
